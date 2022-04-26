fafbc73e67d53ba429a145c1c11dcb40322cf3cc
saikatwtf <84172414+saikatwtf@users.noreply.github.com>
2022-04-26 14:44:20 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,7 +26,7 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
+                           switch_pm_text='Thik hai bhai',
                            switch_pm_parameter="hehe")
         return
 
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('🔍 Search again', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
