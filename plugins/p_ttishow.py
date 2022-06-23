9bec18bb22f12ab0aae16dc4f728be99bd5c6a3d
SaikatFTW <84172414+saikatwtf@users.noreply.github.com>
2022-06-23 12:11:33 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/MoviZenX')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/MoviGenX')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
