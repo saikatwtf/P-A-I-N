3e343c026695b2ead9774836e852e43151f88ad3
SaikatFTW <84172414+saikatwtf@users.noreply.github.com>
2022-07-09 19:54:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -736,17 +736,6 @@ async def auto_filter(client, msg, spoll=False):
             await asyncio.sleep(60)
             await hmm.delete()
             await message.delete()
-        except Exception as e:
-            logger.exception(e)
-            fek = await message.reply_photo(photo="https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg", caption=cap, reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(60)
-            await fek.delete()
-            await msg.delete()
-    else:
-        fuk = await message.reply_photo(photo="https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg", caption=cap, reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(60)
-        await fuk.delete()
-        await msg.delete()
     if spoll:
         await msg.message.delete()
 
